ad124a8064693e50f9967ac0412bd58b4008628b
RyanCaimano <ryancaimano@gmail.com>
2022-09-19 16:49:51 -0500

    The initial setup before creating my routes.

--- app/server-without-libraries.js
+++ app/server-without-libraries.js
@@ -7,60 +7,60 @@ let user = {};
 let categories = [];
 
 http.createServer(function (request, response) {
-  const { headers, method, url } = request;
-  let body = [];
-  request.on('error', (err) => {
-    // Log any errors for now to the console to debug
-    console.error(err);
-  }).on('data', (chunk) => {
-    // Add the data from the next 'chunk' to the data array
-    body.push(chunk);
-  }).on('end', () => {
-    // Put together all the pieces of the request body
-    body = Buffer.concat(body).toString();
+	const { headers, method, url } = request;
+	let body = [];
+	request.on('error', (err) => {
+		// Log any errors for now to the console to debug
+		console.error(err);
+	}).on('data', (chunk) => {
+		// Add the data from the next 'chunk' to the data array
+		body.push(chunk);
+	}).on('end', () => {
+		// Put together all the pieces of the request body
+		body = Buffer.concat(body).toString();
     
-    // Log errors for now
-    response.on('error', (err) => {
-      console.error(err);
-    });
+		// Log errors for now
+		response.on('error', (err) => {
+			console.error(err);
+		});
     
-    // Offload the routing and handling of the api calls to helper function
-    handleApiRequest(body,request,response);
-  });
+		// Offload the routing and handling of the api calls to helper function
+		handleApiRequest(body,request,response);
+	});
 }).listen(3001, ()=> {
-  // Load dummy data into server memory for serving
-  goals = JSON.parse(fs.readFileSync("goals.json","utf-8"));
+	// Load dummy data into server memory for serving
+	goals = JSON.parse(fs.readFileSync('goals.json','utf-8'));
   
-  // Load all users into users array and for now hardcode the first user to be "logged in"
-  users = JSON.parse(fs.readFileSync("users.json","utf-8"));
-  user = users[0];
+	// Load all users into users array and for now hardcode the first user to be "logged in"
+	users = JSON.parse(fs.readFileSync('users.json','utf-8'));
+	user = users[0];
   
-  // Load all categories from file
-  categories = JSON.parse(fs.readFileSync("categories.json","utf-8"));
+	// Load all categories from file
+	categories = JSON.parse(fs.readFileSync('categories.json','utf-8'));
 });
 
 function handleApiRequest(body, request,response) {
-  // Set the default headers on the response
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'application/json');
-  if (request.method === "GET") {
-    // Handle the Get endpoints
-    if (request.url === "/v1/goals") {
-      return response.end(JSON.stringify(goals));
-    } 
-  } else if (request.method === "POST") {
-    // Handle the POST endpoints
-    // Pull out body of request for processing
-    let postBody = JSON.parse(body);
-    // Use regular expressions to pull out url param
-    if (request.url.search('/\/v1\/me\/goals\/*\/accept/')) {
-      let goalId = Number(request.url.match('/\d+$/'));
-      // Make sure goal exists
-      let goal = goals.find((goal)=> {
-        return goal.id == goalId
-      })
-      user.acceptedGoals.push(goal); 
-      return response.end();
-    }
-  }
+	// Set the default headers on the response
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'application/json');
+	if (request.method === 'GET') {
+		// Handle the Get endpoints
+		if (request.url === '/v1/goals') {
+			return response.end(JSON.stringify(goals));
+		} 
+	} else if (request.method === 'POST') {
+		// Handle the POST endpoints
+		// Pull out body of request for processing
+		let postBody = JSON.parse(body);
+		// Use regular expressions to pull out url param
+		if (request.url.search('/\/v1\/me\/goals\/*\/accept/')) {
+			let goalId = Number(request.url.match('/\d+$/'));
+			// Make sure goal exists
+			let goal = goals.find((goal)=> {
+				return goal.id == goalId;
+			});
+			user.acceptedGoals.push(goal); 
+			return response.end();
+		}
+	}
 }
--- app/server.js
+++ app/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const http = require('http');
 const finalHandler = require('finalhandler');
 const queryString = require('querystring');
@@ -5,11 +6,12 @@ const url = require('url');
 const Router = require('router');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+
 // State holding variables
 let goals = [];
 let user = {};
-let users = [];
 let categories = [];
+let users = [];
 
 // Setup router
 let myRouter = Router();
@@ -17,59 +19,66 @@ myRouter.use(bodyParser.json());
 
 // This function is a bit simpler...
 http.createServer(function (request, response) {
-  myRouter(request, response, finalHandler(request, response))
+	myRouter(request, response, finalHandler(request, response));
 }).listen(3001, () => {
-  // Load dummy data into server memory for serving
-  goals = JSON.parse(fs.readFileSync("goals.json","utf-8"));
+	// Load dummy data into server memory for serving
+	goals = JSON.parse(fs.readFileSync('goals.json','utf-8'));
   
-  // Load all users into users array and for now hardcode the first user to be "logged in"
-  users = JSON.parse(fs.readFileSync("users.json","utf-8"));
-  user = users[0];
+	// Load all users into users array and for now hardcode the first user to be "logged in"
+	users = JSON.parse(fs.readFileSync('users.json','utf-8'));
+	user = users[0];
   
-  // Load all categories from file
-  categories = JSON.parse(fs.readFileSync("categories.json","utf-8"));
+	// Load all categories from file
+	categories = JSON.parse(fs.readFileSync('categories.json','utf-8'));
 });
 
 // Notice how much cleaner these endpoint handlers are...
 myRouter.get('/v1/goals', function(request,response) {
-  // Get our query params from the query string
-  const queryParams = queryString.parse(url.parse(request.url).query)
+	// Get our query params from the query string
+	const queryParams = queryString.parse(url.parse(request.url).query);
+	
 
-  // TODO: Do something with the query params
 
-  // Return all our current goal definitions (for now)
-  return response.end(JSON.stringify(goals));
+
+	
+	
+
+
+	// TODO: Do something with the query params
+
+	// Return all our current goal definitions (for now)
+	return response.end(JSON.stringify(goals));
 });
 
 // See how i'm not having to build up the raw data in the body... body parser just gives me the whole thing as an object.
 // See how the router automatically handled the path value and extracted the value for me to use?  How nice!
 myRouter.post('/v1/me/goals/:goalId/accept', function(request,response) {
-  // Find goal from id in url in list of goals
-  let goal = goals.find((goal)=> {
-    return goal.id == request.params.goalId
-  })
-  // Add it to our logged in user's accepted goals
-  user.acceptedGoals.push(goal); 
-  // No response needed other than a 200 success
-  return response.end();
+	// Find goal from id in url in list of goals
+	let goal = goals.find((goal)=> {
+		return goal.id == request.params.goalId;
+	});
+	// Add it to our logged in user's accepted goals
+	user.acceptedGoals.push(goal); 
+	// No response needed other than a 200 success
+	return response.end();
 });
 
 myRouter.post('/v1/me/goals/:goalId/challenge/:userId', function(request,response) {
-  // Find goal from id in url in list of goals
-  let goal = goals.find((goal)=> {
-    return goal.id == request.params.goalId
-  })
-  // Find the user who is being challenged in our list of users
-  let challengedUser = users.find((user)=> {
-    return user.id == request.params.userId
-  })
-  // Make sure the data being changed is valid
-  if (!goal) {
-    response.statusCode = 400
-    return response.end("No goal with that ID found.")
-  }
-  // Add the goal to the challenged user
-  challengedUser.challengedGoals.push(goal); 
-  // No response needed other than a 200 success
-  return response.end();
+	// Find goal from id in url in list of goals
+	let goal = goals.find((goal)=> {
+		return goal.id == request.params.goalId;
+	});
+	// Find the user who is being challenged in our list of users
+	let challengedUser = users.find((user)=> {
+		return user.id == request.params.userId;
+	});
+	// Make sure the data being changed is valid
+	if (!goal) {
+		response.statusCode = 400;
+		return response.end('No goal with that ID found.');
+	}
+	// Add the goal to the challenged user
+	challengedUser.challengedGoals.push(goal); 
+	// No response needed other than a 200 success
+	return response.end();
 });
